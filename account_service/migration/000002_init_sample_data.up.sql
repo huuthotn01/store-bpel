f7fe02a7238f9bcf1a0318d0463cb683a8955f0b
phongtrantuan <73282470+phongtrantuan@users.noreply.github.com>
2023-02-23 00:19:22 +0700

    add list data account (#12)
    Co-authored-by: PhongxTran <92942665+PhongxTran@users.noreply.github.com>

---
+++ account_service/migration/000002_init_sample_data.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO `account` (`username`, `password`, `user_role`, `is_activated`, `created_at`) VALUES
+('admin1', '$2a$10$vscz1U6wyLKfLXyLGYQTVuxwM3l6knEpdDV14XOqTPJ8XFgj5pTti', 2, 0, '2023-02-22 08:17:02'),
+('branch1', '$2a$10$Dm4tAWTGkrTbIlLzrn6PNeE7h1HHO/UzUqhbCur57AKjtZT2eyTQm', 3, 0, '2023-02-22 10:01:54'),
+('goods1', '$2a$10$0NlLr1dBTDL1SLLQIn37xeTB66mUZag3FrDHNP8Y23Bb2q3a9kVYi', 5, 0, '2023-02-22 10:02:21'),
+('lead_branch1', '$2a$10$C2hz9AWaOA4ZM6EYt5Bq7u0UdvstDbicyvVba5Uj/t/bPcL05T83y', 6, 0, '2023-02-22 10:03:10'),
+('user1', '$2a$10$xv8gNKl1kEeL4wSLUh0jyeia10xDkdybDBaw3jbhBHp/JCVkyBGXC', 1, 0, '2023-02-22 08:17:10'),
+('user2', '$2a$10$wOCSPxvyR7AadwaQL7GXHefP8F.879LnHYh3v/bGgR4opQHhIAhK.', 1, 0, '2023-02-22 10:00:36'),
+('warehouse1', '$2a$10$58MY05gTm1PDzc8PMosZqOgDCFyAt//160xfH/JQvcUOagM.69Z.q', 4, 0, '2023-02-22 10:02:07');
