2ccf42c35eb1144620038fc8c66717d9a552511f
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-03-26 22:20:27 +0700

    branch data

--- branch_service/migration/000001_init_branch_service_db.up.sql
+++ branch_service/migration/000001_init_branch_service_db.up.sql
@@ -22,7 +22,7 @@ create table if not exists branch_manager (
     branch_code int,
     manager_code varchar(100),
     start_date timestamp not null default CURRENT_TIMESTAMP,
-    end_date timestamp,
+    end_date timestamp null,
     constraint PK_branch_manager primary key (branch_code, manager_code)
 ) engine = InnoDB default charset = utf8mb4 collate = utf8mb4_unicode_ci ;
 
@@ -30,6 +30,6 @@ create table if not exists branch_staff (
     branch_code int,
     staff_code varchar(100),
     start_date timestamp not null default CURRENT_TIMESTAMP,
-    end_date timestamp,
+    end_date timestamp null,
     constraint PK_branch_staff primary key (branch_code, staff_code)
 ) engine = InnoDB default charset = utf8mb4 collate = utf8mb4_unicode_ci ;
