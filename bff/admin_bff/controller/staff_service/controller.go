08eeaf2feb42cb2b0a249655b21065ffc15a1fe4
huuthotn01 <huuthotn01@gmail.com>
2023-03-27 01:22:48 +0700

    Resolve conversation

--- bff/admin_bff/controller/staff_service/controller.go
+++ bff/admin_bff/controller/staff_service/controller.go
@@ -24,7 +24,7 @@ type staffBffController struct {
 }
 
 func NewController(cfg *config.Config) IStaffBffController {
-	// init branch adapter
+	// init staff adapter
 	staffAdapter := adapter.NewStaffAdapter(cfg)
 
 	return &staffBffController{
