a0209432026f10f9bf239bc25a4ac8bb10d89aa4
Bùi Lương Vinh Hiển <75056137+hienbui-iwrp@users.noreply.github.com>
2023-03-27 17:39:30 +0700

    Sample data (#37)
    * branch data
    
    * staff sample_data
    
    * update get request list

--- branch_service/migration/000002_init_sample_data.up.sql
+++ branch_service/migration/000002_init_sample_data.up.sql
@@ -2,7 +2,7 @@ insert into branch values
     (null, 'Branch 1', 'TP. HCM', 'Q. 10', 'P. 11', 'Ly Thuong Kiet', '2023-01-01 10:00:00', 'staff-1', '07:00:00', '18:00:00');
 
 insert into branch_img values
-    (1, 'img_simu_link');
+    (1, 'https://media.tarkett-image.com/medium/RF_SEE_7_SENSES_STORE_6.jpg');
 
 insert into branch_manager values
     (1, 'staff-1', '2023-01-01 00:00:00', null);
