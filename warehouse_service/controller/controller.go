3f81c501e32373ae5da2e8affa4f3fbb176f5803
huuthotn01 <55121446+huuthotn01@users.noreply.github.com>
2023-03-29 20:48:48 +0700

    Fix wh service (#43)

--- warehouse_service/controller/controller.go
+++ warehouse_service/controller/controller.go
@@ -3,17 +3,17 @@ package controller
 import (
 	"context"
 	"gorm.io/gorm"
-	"store-bpel/staff_service/schema"
 	"store-bpel/warehouse_service/adapter"
 	"store-bpel/warehouse_service/config"
+	"store-bpel/warehouse_service/schema"
 )
 
 type IWarehouseServiceController interface {
-	GetWarehouseStaff(ctx context.Context) (*schema.GetStaffResponse, error)
+	GetWarehouseStaff(ctx context.Context) ([]*schema.GetWarehouseStaffResponseData, error)
 }
 
 type warehouseServiceController struct {
-	config *config.Config
+	config     *config.Config
 	repository *gorm.DB
 
 	staffAdapter adapter.IStaffServiceAdapter
@@ -24,8 +24,8 @@ func NewController(config *config.Config, db *gorm.DB) IWarehouseServiceControll
 	staffAdapter := adapter.NewStaffAdapter(config)
 
 	return &warehouseServiceController{
-		config: config,
-		repository: db,
+		config:       config,
+		repository:   db,
 		staffAdapter: staffAdapter,
 	}
 }
