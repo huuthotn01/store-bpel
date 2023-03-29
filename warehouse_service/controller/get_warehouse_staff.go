69dd27a76545fa2e632a1d5de5d3b5ec3e9d7c7e
huuthotn01 <huuthotn01@gmail.com>
2023-03-29 20:47:56 +0700

    Fix wh service

--- warehouse_service/controller/get_warehouse_staff.go
+++ warehouse_service/controller/get_warehouse_staff.go
@@ -2,13 +2,37 @@ package controller
 
 import (
 	"context"
-	"store-bpel/staff_service/schema"
+	"store-bpel/warehouse_service/schema"
 )
 
-func (s *warehouseServiceController) GetWarehouseStaff(ctx context.Context) (*schema.GetStaffResponse, error) {
+func (s *warehouseServiceController) GetWarehouseStaff(ctx context.Context) ([]*schema.GetWarehouseStaffResponseData, error) {
 	staffs, err := s.staffAdapter.GetStaff(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return staffs, nil
+
+	respStaffs := make([]*schema.GetWarehouseStaffResponseData, 0, len(staffs))
+	for _, data := range staffs {
+		respStaffs = append(respStaffs, &schema.GetWarehouseStaffResponseData{
+			StaffId:     data.StaffId,
+			StaffName:   data.StaffName,
+			Street:      data.Street,
+			Ward:        data.Ward,
+			District:    data.District,
+			Province:    data.Province,
+			CitizenId:   data.CitizenId,
+			Role:        data.Role,
+			BranchId:    data.BranchId,
+			Hometown:    data.Hometown,
+			Salary:      data.Salary,
+			Birthdate:   data.Birthdate,
+			StartDate:   data.StartDate,
+			Gender:      data.Gender,
+			PhoneNumber: data.PhoneNumber,
+			Status:      data.Status,
+			Email:       data.Email,
+		})
+	}
+
+	return respStaffs, nil
 }
--- warehouse_service/schema/response.go
+++ warehouse_service/schema/response.go
@@ -1,6 +1,34 @@
 package schema
 
+import "time"
+
 type UpdateResponse struct {
 	StatusCode int
-	Message string
+	Message    string
+}
+
+type GetWarehouseStaffResponse struct {
+	StatusCode int
+	Message    string
+	Data       []*GetWarehouseStaffResponseData
+}
+
+type GetWarehouseStaffResponseData struct {
+	StaffId     string
+	StaffName   string
+	Street      string
+	Ward        string
+	District    string
+	Province    string
+	CitizenId   string
+	Role        string
+	BranchId    string
+	Hometown    string
+	Salary      int
+	Birthdate   string
+	StartDate   time.Time
+	Gender      string
+	PhoneNumber string
+	Status      string
+	Email       string
 }
