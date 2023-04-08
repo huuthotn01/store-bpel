4c1f695fa80ccf88f6c1cc127d23079f38ef1c5c
Bùi Lương Vinh Hiển <75056137+hienbui-iwrp@users.noreply.github.com>
2023-04-08 19:41:47 +0700

    update limit quantity (#77)

--- cart_service/controller/update_goods.go
+++ cart_service/controller/update_goods.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"errors"
 	"store-bpel/cart_service/repository"
 	"store-bpel/cart_service/schema"
 )
@@ -9,6 +10,14 @@ import (
 func (s *cartServiceController) UpdateGoods(ctx context.Context, cartId string, request []*schema.AddGoodsRequest) error {
 	var goodsList []*repository.AddGoodsData
 	for _, goods := range request {
+		maxQuantity, err := s.getMaxQuantity(ctx, goods)
+		if err != nil {
+			return err
+		}
+
+		if goods.Quantity > maxQuantity {
+			return errors.New("UpdateGoods-quantity limit exceeded")
+		}
 		goodsList = append(goodsList, &repository.AddGoodsData{
 			GoodsId:    goods.GoodsId,
 			GoodsSize:  goods.GoodsSize,
