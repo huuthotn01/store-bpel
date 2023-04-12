7c69b0cd0e31a52ab4a624bbd59f307d77893438
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-04-12 21:50:32 +0700

    update order data

--- order_service/migration/000002_init_sample_data.up.sql
+++ order_service/migration/000002_init_sample_data.up.sql
@@ -2,3 +2,56 @@ insert into order_state(order_code, state) values
     (1, 'NHÀ SẢN XUẤT CHUẨN BỊ HÀNG'),
     (2, 'NHÀ SẢN XUẤT CHUẨN BỊ HÀNG'),
     (1, 'NHẬP KHO BÌNH THẠNH') ;
+
+
+INSERT INTO `goods` (`goods_code`, `goods_size`, `goods_color`, `goods_name`, `order_code`, `quantity`, `unit_price`, `total_price`, `tax`, `image`, `promotion`) VALUES
+('goods_1681248800', '31', 'Đen ', '', 1, 2, 80000, 72000, 1000, '', 10),
+('goods_1681248800', '31', 'Đen ', '', 2, 1, 80000, 72000, 1000, '', 10),
+('goods_1681248800', '32', 'Đen ', '', 1, 1, 80000, 72000, 1000, '', 10),
+('goods_1681248800', '32', 'Đen ', '', 2, 1, 80000, 72000, 1000, '', 10),
+('goods_1681248884', '36', 'Đỏ xanh', '', 3, 1, 100000, 100000, 1000, '', 0),
+('goods_1681248884', '36', 'Trắng xanh', '', 3, 1, 100000, 100000, 1000, '', 0),
+('goods_1681249061', 'L', 'Đen', '', 4, 1, 185000, 185000, 1000, '', 0),
+('goods_1681249061', 'L', 'Hồng', '', 4, 1, 185000, 185000, 1000, '', 0),
+('goods_1681249061', 'M', 'Đen', '', 4, 1, 185000, 185000, 1000, '', 0),
+('goods_1681249162', 'L', 'Trắng hồng', '', 5, 1, 376000, 376000, 1000, '', 0),
+('goods_1681249286', '37', 'Đen', '', 6, 2, 249000, 249000, 1000, '', 0),
+('goods_1681249286', '38', 'Đen', '', 7, 1, 249000, 249000, 1000, '', 0),
+('goods_1681249286', '38', 'Trắng', '', 7, 1, 249000, 249000, 1000, '', 0),
+('goods-1', 'M', 'Đen', '', 8, 1, 250000, 250000, 1000, '', 0),
+('goods-2', 'L', 'Vàng', '', 10, 2, 150000, 150000, 1000, '', 0),
+('goods-2', 'M', 'Vàng', '', 10, 2, 150000, 150000, 1000, '', 0),
+('goods-3', 'L', 'Trắng', '', 11, 4, 200000, 200000, 1000, '', 0),
+('goods-4', 'XL', 'Trắng', '', 12, 1, 100000, 100000, 1000, '', 0),
+('goods-5', 'S', 'Đỏ', '', 9, 2, 120000, 120000, 1000, '', 0);
+
+INSERT INTO `online_orders` (`order_code`, `expected_delivery`, `shipping_fee`, `customer_id`, `payment_method`, `street`, `ward`, `district`, `province`, `customer_name`, `customer_phone`, `customer_email`, `status`) VALUES
+(1, '2023-04-06', 5000, 'user2', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen Huu Tho', '0123456789', 'test@gmail.com', 0),
+(2, '2023-04-06', 5000, 'user2', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen Huu Tho', '0123456789', 'test@gmail.com', 0),
+(3, '2023-04-06', 5000, 'user1', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen Huu', '0123456789', 'testT@gmail.com', 0),
+(4, '2023-04-07', 5000, 'user1', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen Hu', '0123456789', 'testT1@gmail.com', 0),
+(5, '2023-04-07', 5000, 'user1', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen', '0123456789', 'tes1@gmail.com', 0),
+(6, '2023-04-07', 5000, 'user1', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen', '0123456789', 'tes1@gmail.com', 0),
+(7, '2023-04-10', 5000, 'user2', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran Nguyen', '0123456789', 'tes1@gmail.com', 0),
+(8, '2023-04-10', 5000, 'user1', 'Momo', 'Nguyen Du', 'Dong Hoa', 'Di An', 'Binh Duong', 'Tran N', '0123456789', 't211@gmail.com', 0);
+
+
+INSERT INTO `orders` (`order_code`, `transaction_date`, `total_price`, `public_order_code`) VALUES
+(1, '2023-04-01 17:00:00', 216000, 'nxfbAUZh'),
+(2, '2023-03-31 17:00:00', 144000, 'fqNVgCDe'),
+(3, '2023-03-31 17:00:00', 200000, 'izLuKevz'),
+(4, '2023-04-02 17:00:00', 555000, 'uCYnYnZL'),
+(5, '2023-04-03 17:00:00', 376000, 'jTZzdFPk'),
+(6, '2023-04-04 17:00:00', 498000, 'tyzkMbbA'),
+(7, '2023-04-05 17:00:00', 498000, 'hNSmlsxW'),
+(8, '2023-04-06 17:00:00', 250000, 'NKiueqgi'),
+(9, '2023-04-07 17:00:00', 240000, 'IxpNHEvR'),
+(10, '2023-04-08 17:00:00', 600000, 'lcNKwioP'),
+(11, '2023-04-09 17:00:00', 800000, 'EJNrGYpD'),
+(12, '2023-04-10 17:00:00', 100000, 'cNvWGctG');
+
+INSERT INTO `store_orders` (`order_code`, `store_code`, `staff_id`) VALUES
+(9, '1', '12345'),
+(10, '1', '1'),
+(11, '1', '1'),
+(12, '1', '3');
--- staff_service/migration/000002_init_sample_data.up.sql
+++ staff_service/migration/000002_init_sample_data.up.sql
@@ -11,18 +11,23 @@ INSERT INTO `attendance` (`staff_id`, `attendance_date`, `checkin_time`, `checko
 ('12345', '2023-02-03', '2023-02-17 00:00:00', '2023-02-01 11:00:30');
 
 INSERT INTO `requests` (`id`, `request_date`, `request_type`, `staff_id`, `status`) VALUES
-('1', '2023-02-09', 'ADD', '1', 'PENDING'),
-('3', '2023-02-09', 'DELETE', '12345', 'PENDING');
+('1', '2023-02-09', 'ADD', '1', 'APPROVED'),
+('3', '2023-02-09', 'DELETE', '12345', 'UNAPPROVED'),
+('add_1681309271', '2023-04-12', 'ADD', 'd', 'PENDING'),
+('del_1681309108', '2023-04-12', 'DELETE', 'hoLe', 'PENDING');
+
 
 INSERT INTO `staff` (`staff_id`, `staff_name`, `province`, `district`, `ward`, `street`, `birthdate`, `hometown`, `citizen_id`, `staff_position`, `start_date`, `salary`, `gender`, `phone`, `email`, `branch_id`, `status`) VALUES
-('1', 'aaaa', 'a', 'b', 'x', 'd', '2023-02-01', 'DL', '038746453', '7', '2023-02-17 07:53:25', 1200000, 'FEMALE', '0294756342', 'aaaa@gmail.com', '1', 'PENDING'),
-('12345', 'Bùi Hiển', 'Bình dương', 'Dĩ An', 'Đông Hòa', 'Lương Định Của', '2000-02-22', 'Daklak', '0192756559', '7', '2023-01-31 10:00:00', 1000000, 'MALE', '0294756342', 'a@gmail.com', '1', 'APPROVED'),
-('3', 'sdfg', 'a', 'b', 'x', 'd', '2023-02-01', 'DL', '038746873', '7', '2023-02-17 07:54:03', 1200000, 'FEMALE', '0294756342', '3@gmail.com', '1', 'PENDING'),
-('admin1', 'admin', 'Lương Định Của', 'Đông Hòa', 'Dĩ An', 'Bình Dương', '2001-07-19', 'DakLak', '0192756559343', '2', '2023-03-21 08:09:18', 123123344, 'UNDEFINED', '04536821', 'admin@gmail.com', '2', 'APPROVED'),
-('branch1', 'branch1 Bùi', 'Võ Văn Ngân', '11', 'Thủ Đức', 'HCM', '1995-03-09', 'Đà Lạt', '3772487934512', '3', '2023-03-27 04:28:40', 12000000, 'MALE', '0345288743', 'branch1@gmail.com', NULL, 'APPROVED'),
-('goods1', 'goods1 Trần', 'Võ Văn Ngân', '14', 'Dĩ An', 'Bình Dương', '1995-03-20', 'Đà Lạt', '3772487934334', '5', '2023-03-27 04:32:04', 13000000, 'MALE', '03452846743', 'goods1@gmail.com', NULL, 'APPROVED'),
-('lead_branch1', 'lead_branch1 Nguyễn', 'Tô Ngọc Vân', '11', 'Thủ Đức', 'HCM', '1998-09-09', 'Bình Phước', '37722357934512', '6', '2023-03-27 04:32:04', 10000000, 'FEMALE', '0345288743', 'lead_branch1@gmail.com', '2', 'APPROVED'),
-('warehouse1', 'warehouse1 Lê', 'Võ Văn Ngân', '11', 'Thủ Đức', 'HCM', '1995-03-09', 'Đà Lạt', '3776487934512', '4', '2023-03-27 04:32:05', 11000000, 'UNDEFINED', '0345288743', 'warehouse1@gmail.com', NULL, 'APPROVED'),
-('staff-w1', 'Phan Anh', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-02-22', 'Phú Yêu', '484638345245', '7', '2023-04-01 04:27:54', 1200000, 'FEMALE', '043456563', 'w1@gmail.com', NULL, 'APPROVED'),
-('staff-w2', 'Đinh L', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-02-26', 'Tây Ninh', '484638334245', '7', '2023-04-01 04:29:31', 2200000, 'FEMALE', '043455563', 'w2@gmail.com', NULL, 'APPROVED'),
-('staff-w3', 'Trương M', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-12-22', 'Phú Yêu', '486638345245', '7', '2023-04-01 04:29:32', 3200000, 'FEMALE', '043457563', 'w3@gmail.com', NULL, 'APPROVED');
+('1', 'aaaa', 'a', 'b', 'x', 'd', '2023-02-01', 'DL', '038746453', '7', '2023-02-17 00:53:25', 1200000, 'FEMALE', '0294756342', 'aaaa@gmail.com', '1', 'PENDING'),
+('12345', 'Bùi Hiển', 'Bình dương', 'Dĩ An', 'Đông Hòa', 'Lương Định Của', '2000-02-22', 'Daklak', '0192756559', '7', '2023-01-31 03:00:00', 1000000, 'MALE', '0294756342', 'a@gmail.com', '1', 'APPROVED'),
+('3', 'sdfg', 'a', 'b', 'x', 'd', '2023-02-01', 'DL', '038746873', '7', '2023-02-17 00:54:03', 1200000, 'FEMALE', '0294756342', '3@gmail.com', '1', 'PENDING'),
+('admin1', 'admin', 'Lương Định Của', 'Đông Hòa', 'Dĩ An', 'Bình Dương', '2001-07-19', 'DakLak', '0192756559343', '2', '2023-03-21 01:09:18', 123123344, 'UNDEFINED', '04536821', 'admin@gmail.com', '2', 'APPROVED'),
+('branch1', 'branch1 Bùi', 'Võ Văn Ngân', '11', 'Thủ Đức', 'HCM', '1995-03-09', 'Đà Lạt', '3772487934512', '3', '2023-03-26 21:28:40', 12000000, 'MALE', '0345288743', 'branch1@gmail.com', NULL, 'APPROVED'),
+('d', 'Võ Ngọc D', 'Hồ Chí Minh', '10', '14', 'Tô Hiến Thành', '2004-04-09', 'Bình Dương', '02948572342112', '7', '2023-04-12 14:15:38', 500000, 'FEMALE', '0475638354', 'd@gmail.com', '2', 'PENDING'),
+('goods1', 'goods1 Trần', 'Võ Văn Ngân', '14', 'Dĩ An', 'Bình Dương', '1995-03-20', 'Đà Lạt', '3772487934334', '5', '2023-03-26 21:32:04', 13000000, 'MALE', '03452846743', 'goods1@gmail.com', NULL, 'APPROVED'),
+('hoLe', 'Lê Ngọc Hồ', 'Hồ Chí Minh', '10', '14', 'Tô Hiến Thành', '1994-04-30', 'Bình Dương', '0294852342112', '7', '2023-04-12 14:17:15', 500000, 'FEMALE', '0475633354', 'hoLe@gmail.com', '3', 'APPROVED'),
+('lead_branch1', 'lead_branch1 Nguyễn', 'Tô Ngọc Vân', '11', 'Thủ Đức', 'HCM', '1998-09-09', 'Bình Phước', '37722357934512', '6', '2023-03-26 21:32:04', 10000000, 'FEMALE', '0345288743', 'lead_branch1@gmail.com', '2', 'APPROVED'),
+('staff-w1', 'Phan Anh', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-02-22', 'Phú Yêu', '484638345245', '7', '2023-03-31 21:27:54', 1200000, 'FEMALE', '043456563', 'w1@gmail.com', NULL, 'APPROVED'),
+('staff-w2', 'Đinh L', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-02-26', 'Tây Ninh', '484638334245', '7', '2023-03-31 21:29:31', 2200000, 'FEMALE', '043455563', 'w2@gmail.com', NULL, 'APPROVED'),
+('staff-w3', 'Trương M', 'Bình dương', 'Dĩ An', 'Tân Hòa', 'Nguyễn Thái Học', '2001-12-22', 'Phú Yêu', '486638345245', '7', '2023-03-31 21:29:32', 3200000, 'FEMALE', '043457563', 'w3@gmail.com', NULL, 'APPROVED'),
+('warehouse1', 'warehouse1 Lê', 'Võ Văn Ngân', '11', 'Thủ Đức', 'HCM', '1995-03-09', 'Đà Lạt', '3776487934512', '4', '2023-03-26 21:32:05', 11000000, 'UNDEFINED', '0345288743', 'warehouse1@gmail.com', NULL, 'APPROVED');
