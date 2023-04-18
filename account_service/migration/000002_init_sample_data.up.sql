987d1ac67db3ad532ed96e924bdcdf26d56480e9
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-04-18 21:29:24 +0700

    update AddStaff, UpdateStaff

--- account_service/migration/000002_init_sample_data.up.sql
+++ account_service/migration/000002_init_sample_data.up.sql
@@ -1,8 +1,18 @@
-INSERT INTO `account` (`username`, `password`, `user_role`, `created_at`) VALUES
-('admin1', '$2a$10$vscz1U6wyLKfLXyLGYQTVuxwM3l6knEpdDV14XOqTPJ8XFgj5pTti', 2, '2023-02-22 08:17:02'),
-('branch1', '$2a$10$Dm4tAWTGkrTbIlLzrn6PNeE7h1HHO/UzUqhbCur57AKjtZT2eyTQm', 3, '2023-02-22 10:01:54'),
-('goods1', '$2a$10$0NlLr1dBTDL1SLLQIn37xeTB66mUZag3FrDHNP8Y23Bb2q3a9kVYi', 5, '2023-02-22 10:02:21'),
-('lead_branch1', '$2a$10$C2hz9AWaOA4ZM6EYt5Bq7u0UdvstDbicyvVba5Uj/t/bPcL05T83y', 6, '2023-02-22 10:03:10'),
-('user1', '$2a$10$xv8gNKl1kEeL4wSLUh0jyeia10xDkdybDBaw3jbhBHp/JCVkyBGXC', 1, '2023-02-22 08:17:10'),
-('user2', '$2a$10$wOCSPxvyR7AadwaQL7GXHefP8F.879LnHYh3v/bGgR4opQHhIAhK.', 1, '2023-02-22 10:00:36'),
-('warehouse1', '$2a$10$58MY05gTm1PDzc8PMosZqOgDCFyAt//160xfH/JQvcUOagM.69Z.q', 4, '2023-02-22 10:02:07');
+
+
+INSERT INTO `account` (`username`, `password`, `user_role`, `is_activated`, `created_at`) VALUES
+('1', '$2a$10$3wcOTc260oTmn1LqXyA4AOMYQdKzScoH62bFUb0UOmJfuhUJc3wty', 7, 0, '2023-04-18 14:22:57'),
+('12345', '$2a$10$g67JKL1U65rcq.kiEhBh0u3jGn1Uz/a2.N9f1QQA4YshxQKsQrwSi', 7, 0, '2023-04-18 14:23:18'),
+('3', '$2a$10$z/FrGJncJ2Snmkq/Oqjsae9kLijusygGgjqkIozSjKzdAHxELkc1K', 7, 0, '2023-04-18 14:23:22'),
+('admin1', '$2a$10$vscz1U6wyLKfLXyLGYQTVuxwM3l6knEpdDV14XOqTPJ8XFgj5pTti', 2, 1, '2023-02-22 01:17:02'),
+('branch1', '$2a$10$Dm4tAWTGkrTbIlLzrn6PNeE7h1HHO/UzUqhbCur57AKjtZT2eyTQm', 3, 1, '2023-02-22 03:01:54'),
+('d', '$2a$10$nqMK/.GXT2YEJdG833mhcehDloU4thjjn9va/yQO7.YMyLVS0go1O', 7, 0, '2023-04-18 14:23:28'),
+('goods1', '$2a$10$0NlLr1dBTDL1SLLQIn37xeTB66mUZag3FrDHNP8Y23Bb2q3a9kVYi', 5, 1, '2023-02-22 03:02:21'),
+('hoLe', '$2a$10$vZrYZzqYkrTnDoD45AuAXO.YV/6cvlzdt3lGhhg4JZZGy9oFggdCe', 7, 0, '2023-04-18 14:23:37'),
+('lead_branch1', '$2a$10$C2hz9AWaOA4ZM6EYt5Bq7u0UdvstDbicyvVba5Uj/t/bPcL05T83y', 6, 1, '2023-02-22 03:03:10'),
+('staff-w1', '$2a$10$WWCP5kg7WoIDoVwGMcxysOVxoyiRHCWMX82wXNvJ0gwaDwifWz0c2', 7, 0, '2023-04-18 14:23:48'),
+('staff-w2', '$2a$10$Qc9pZBGKQEsH4iA8HYHUDez2YH7HbP.6JqevgNhZUUKkUNuN/h5eC', 7, 0, '2023-04-18 14:23:50'),
+('staff-w3', '$2a$10$hWeACeT6j2BeApIAWOpYq.Tl6UVWo2h4F5l.5Efcni1iOLQyqXFgu', 7, 0, '2023-04-18 14:23:52'),
+('user1', '$2a$10$xv8gNKl1kEeL4wSLUh0jyeia10xDkdybDBaw3jbhBHp/JCVkyBGXC', 1, 1, '2023-02-22 01:17:10'),
+('user2', '$2a$10$wOCSPxvyR7AadwaQL7GXHefP8F.879LnHYh3v/bGgR4opQHhIAhK.', 1, 1, '2023-02-22 03:00:36'),
+('warehouse1', '$2a$10$58MY05gTm1PDzc8PMosZqOgDCFyAt//160xfH/JQvcUOagM.69Z.q', 4, 1, '2023-02-22 03:02:07');
