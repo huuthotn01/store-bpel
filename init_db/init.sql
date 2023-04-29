5cfdf790c39e51bc368e334878302e74e7796ac2
huuthotn01 <55121446+huuthotn01@users.noreply.github.com>
2023-04-29 17:59:30 +0700

    Add event service db migration (#123)

---
+++ init_db/init.sql
@@ -0,0 +1,23 @@
+create user bpel identified by 'bpel' ;
+
+grant all privileges on *.* to 'bpel' with grant option ;
+
+create database account_service ;
+
+create database branch_service ;
+
+create database cart_service ;
+
+create database customer_service ;
+
+create database event_service ;
+
+create database goods_service ;
+
+create database order_service ;
+
+create database staff_service ;
+
+create database statistic_service ;
+
+create database warehouse_service ;
