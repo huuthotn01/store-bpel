7de121d8e929ddd7a85d8cac7f062848d624dc6e
hienbui-iwrp <hien.bui250801@gmail.com>
2023-05-08 15:14:42 +0700

    update sample data

--- account_service/migration/000002_init_sample_data.up.sql
+++ account_service/migration/000002_init_sample_data.up.sql
@@ -1,9 +1,12 @@
-INSERT INTO `account` (`username`, `password`, `user_role`,`email`, `is_activated`, `created_at`) VALUES
-('admin', '$2a$10$hEIioZ3xNI/5Iocv3Orbxe9K/pI40G.kjzbiZ6yffXfpM1IQc7hvK', 2, 'admin@gmail.com', 1, '2023-04-26 01:43:43'),
-('anLe', '$2a$10$NjemAHQyZ1K.asvcCXR.4uBTZp6veirSUuU94YDDJIdpbX8DTY.BS', 7, 'anLe@gmail.com', 0, '2023-04-26 10:05:02'),
-('aTran', '$2a$10$WTxRXtxjCDhsC2I/EyeMwO2pcmtDK3m7ORzz7ga/q9WlDn7ZiV.3u', 7, 'aTran@gmail.com', 0, '2023-04-26 09:59:00'),
-('binhMac', '$2a$10$j7XxFXe7bFP2RrTD3aeMZuZWqpyg6EWCinloCe67jMaADZVzMVNW2', 7, 'binhMac@gmail.com', 0, '2023-04-26 10:10:06'),
-('ducbinh', '$2a$10$H.qQdOg5Vezj.nM6G9b5B.1FVeY4Sh1m.YqrkFB//5uW3ML9uYb/K', 3, 'ducbinh@gmail.com', 1, '2023-04-26 02:39:55'),
-('lydinh', '$2a$10$5t6tGa4qZJdwN3X7dznpYewxM2l0zw1/bw.wW6kWeR4WdogmVCFS2', 7, 'lydinh@gmail.com', 0, '2023-04-26 10:07:43'),
-('nguyen', '$2a$10$sMoaq5knU1csXKi0zlm/Wu3xizS1vLEN9jwjedummhtdnZlnC/UgC', 6, 'nguyen@gmail.com', 1, '2023-04-26 10:12:38'),
-('quyenBui', '$2a$10$gutoesR.MiBWu/NqSQxjFu0wwq5nZJooE4KAGOuy1uCRkP2S8Oji2', 6, 'quyenBui@gmail.com', 1, '2023-04-26 03:06:21');
+INSERT INTO `account` (`username`, `password`, `user_role`, `email`, `otp`, `otp_timeout`, `is_activated`, `created_at`) VALUES
+('admin', '$2a$10$hEIioZ3xNI/5Iocv3Orbxe9K/pI40G.kjzbiZ6yffXfpM1IQc7hvK', 2, 'admin@gmail.com', NULL, NULL, 1, '2023-04-25 18:43:43'),
+('anLe', '$2a$10$NjemAHQyZ1K.asvcCXR.4uBTZp6veirSUuU94YDDJIdpbX8DTY.BS', 7, 'anLe@gmail.com', NULL, NULL, 0, '2023-04-26 03:05:02'),
+('aTran', '$2a$10$WTxRXtxjCDhsC2I/EyeMwO2pcmtDK3m7ORzz7ga/q9WlDn7ZiV.3u', 7, 'aTran@gmail.com', NULL, NULL, 0, '2023-04-26 02:59:00'),
+('binhMac', '$2a$10$j7XxFXe7bFP2RrTD3aeMZuZWqpyg6EWCinloCe67jMaADZVzMVNW2', 7, 'binhMac@gmail.com', NULL, NULL, 0, '2023-04-26 03:10:06'),
+('ducbinh', '$2a$10$H.qQdOg5Vezj.nM6G9b5B.1FVeY4Sh1m.YqrkFB//5uW3ML9uYb/K', 3, 'ducbinh@gmail.com', NULL, NULL, 1, '2023-04-25 19:39:55'),
+('lydinh', '$2a$10$5t6tGa4qZJdwN3X7dznpYewxM2l0zw1/bw.wW6kWeR4WdogmVCFS2', 7, 'lydinh@gmail.com', NULL, NULL, 0, '2023-04-26 03:07:43'),
+('nguyen', '$2a$10$sMoaq5knU1csXKi0zlm/Wu3xizS1vLEN9jwjedummhtdnZlnC/UgC', 6, 'nguyen@gmail.com', NULL, NULL, 1, '2023-04-26 03:12:38'),
+('quyenBui', '$2a$10$gutoesR.MiBWu/NqSQxjFu0wwq5nZJooE4KAGOuy1uCRkP2S8Oji2', 6, 'quyenBui@gmail.com', NULL, NULL, 1, '2023-04-25 20:06:21'),
+('user', '$2a$10$0zpLmzIMjFN3xfA6QOYEVuPdkLIK3qGp59sOCiGcrT/sInWt.b88.', 1, 'phong@gmail.com', NULL, NULL, 1, '2023-05-05 02:38:50'),
+('user1', '$2a$10$iN67QfCB/oLvPX.jurVzKe0zAEckl.pUolvUTS.NPeqP7KMjFUwiu', 1, 'tuanphongpbc@gmail.com', '599438', '2023-05-03 16:19:21', 1, '2023-05-03 16:15:48'),
+('user2', '$2a$10$C56jjsMJslVM13JTkC8ZbOjCw9eYpSfVV2kU1F.0xLhiemp1M7pA.', 1, 'phong.tran6012.1@hcmut.edu.vn', '939371', '2023-05-05 03:55:21', 1, '2023-05-03 16:28:47');
