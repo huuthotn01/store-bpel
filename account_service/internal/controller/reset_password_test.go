1be7d2ff7d18861629a909a0fa565717c560781b
huuthotn01 <55121446+huuthotn01@users.noreply.github.com>
2023-05-08 08:03:17 +0700

    Add account service unit tests (#131)

---
+++ account_service/internal/controller/reset_password_test.go
@@ -0,0 +1,95 @@
+package controller
+
+import (
+	"context"
+	"store-bpel/account_service/config"
+	"store-bpel/account_service/schema"
+	"testing"
+)
+
+func Test_accountServiceController_CreateResetPassword(t *testing.T) {
+	type args struct {
+		request *schema.CreateResetPasswordRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Should create reset password successfully",
+			args: args{
+				request: &schema.CreateResetPasswordRequest{
+					Username: "test-user",
+				},
+			},
+		},
+	}
+
+	ctx := context.Background()
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &accountServiceController{
+				cfg:        cfg,
+				repository: testRepository,
+			}
+			if err := c.CreateResetPassword(ctx, tt.args.request); (err != nil) != tt.wantErr {
+				t.Errorf("CreateResetPassword() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_accountServiceController_ConfirmOTP(t *testing.T) {
+	type args struct {
+		request *schema.ConfirmOTPRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Should confirm OTP and gen new password successfully",
+			args: args{
+				request: &schema.ConfirmOTPRequest{
+					Username: "test-user",
+					Otp:      "test-otp",
+				},
+			},
+		},
+		{
+			name: "Should return error when OTP not correct",
+			args: args{
+				request: &schema.ConfirmOTPRequest{
+					Username: "test-user",
+					Otp:      "invalid-otp",
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	ctx := context.Background()
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &accountServiceController{
+				cfg:        cfg,
+				repository: testRepository,
+			}
+			if err := c.ConfirmOTP(ctx, tt.args.request); (err != nil) != tt.wantErr {
+				t.Errorf("ConfirmOTP() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
