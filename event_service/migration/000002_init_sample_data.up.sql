aa43f839a93103fd532d2e8351f3993b09a72eb7
huuthotn01 <huuthotn01@gmail.com>
2023-05-28 03:13:12 +0700

    Update sample data for event and goods, remove uploads folder in admin bff

--- event_service/migration/000002_init_sample_data.up.sql
+++ event_service/migration/000002_init_sample_data.up.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO `event` (`event_id`, `name`, `discount`, `start_time`, `end_time`, `created_at`, `image`) VALUES
-('event_1682582427', 'Sale áo mùa hè', 0.1, '2023-04-28 10:00:00', '2023-06-01 10:00:00', '2023-04-27 08:00:27', '/store-bpel/bff/admin_bff/uploads/event_1682582427/1682582433.webp'),
-('event_1682582515', 'Mùa hè năng động', 0.08, '2023-05-16 10:00:00', '2023-06-01 10:00:00', '2023-04-27 08:01:55', '/store-bpel/bff/admin_bff/uploads/event_1682582515/1682582570.webp'),
-('event_1682582974', 'Sale phụ kiện nữ', 0.12, '2023-05-03 10:00:00', '2023-05-18 10:00:00', '2023-04-27 08:09:34', '/store-bpel/bff/admin_bff/uploads/event_1682582974/1682582983.webp');
+('event_1682582427', 'Sale áo mùa hè', 0.1, '2023-04-28 10:00:00', '2023-06-01 10:00:00', '2023-04-27 08:00:27', 'https://bpel-bucket.s3.us-west-2.amazonaws.com/event_1682582427/1682582433.webp'),
+('event_1682582515', 'Mùa hè năng động', 0.08, '2023-05-16 10:00:00', '2023-06-01 10:00:00', '2023-04-27 08:01:55', 'https://bpel-bucket.s3.us-west-2.amazonaws.com/event_1682582515/1682582570.webp'),
+('event_1682582974', 'Sale phụ kiện nữ', 0.12, '2023-05-03 10:00:00', '2023-05-18 10:00:00', '2023-04-27 08:09:34', 'https://bpel-bucket.s3.us-west-2.amazonaws.com/event_1682582974/1682582983.webp');
 
 INSERT INTO `goods` (`event_id`, `goods_id`) VALUES
 ('event_1682582427', 'goods_1682531177'),
